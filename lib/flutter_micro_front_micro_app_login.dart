ab01353eb55b42a8fc9a587bd64a07edfc173c8e
Cleiton Silva Rego <cleiton_.98@hotmail.com>
2021-11-23 22:26:46 -0300

    primeira versao do micro app login

--- lib/flutter_micro_front_micro_app_login.dart
+++ lib/flutter_micro_front_micro_app_login.dart
@@ -1,7 +1,1 @@
-library flutter_micro_front_micro_app_login;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'app/micro_app_login_resolver.dart';
