9e373835a65b213b53e590343717f8c17c36d27f
Cleiton Silva Rego <cleiton_.98@hotmail.com>
2021-11-28 21:37:24 -0300

    ajuste page

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_micro_front_micro_commons_desing_system/flutter_micro_front_micro_commons_desing_system.dart';
+import 'package:flutter_micro_front_micro_core/micro_core.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,6 +11,19 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            // Text(_appName, style: TextStyle(fontSize: 20)),
+            // Text(_suffixName, style: TextStyle(fontSize: 20)),
+            // Text(_server, style: TextStyle(fontSize: 20)),
+            CustomButtonWidget(
+              text: 'goToHome',
+              onPressed: () => navigatorKey.currentState?.pushNamed('/home'),
+            ),
+          ],
+        ),
       ),
     );
   }
